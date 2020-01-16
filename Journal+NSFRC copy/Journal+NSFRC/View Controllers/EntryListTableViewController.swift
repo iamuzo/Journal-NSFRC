f0618852d189def38d88e8997a1ae8f934331737
IamUzo <iamuzoagu@gmail.com>
2020-01-16 11:40:25 -0700

    converted from NSFetchRequest to NSFetchedResults

--- Journal+NSFRC copy/Journal+NSFRC/View Controllers/EntryListTableViewController.swift
+++ Journal+NSFRC copy/Journal+NSFRC/View Controllers/EntryListTableViewController.swift
@@ -13,6 +13,7 @@ class EntryListTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        EntryController.sharedInstance.fetchedResultsController.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -21,49 +22,74 @@ class EntryListTableViewController: UITableViewController {
     }
     
     // MARK: - Table view data source
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return EntryController.sharedInstance.entries.count
+        EntryController.sharedInstance.fetchedResultsController.fetchedObjects?.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
 
-        let entry = EntryController.sharedInstance.entries[indexPath.row]
-       guard let entryDate = entry.timestamp else { return UITableViewCell() }
+        guard let entry = EntryController.sharedInstance.fetchedResultsController.fetchedObjects?[indexPath.row], let entryDate = entry.timestamp else { return UITableViewCell() }
+        
         let formattedEntryDate = DateFormatter.localizedString(from: entryDate, dateStyle: .short, timeStyle: .short)
         
         cell.textLabel?.text = entry.title
         cell.detailTextLabel?.text = formattedEntryDate
-        
-        // Configure the cell...
-        
         return cell
     }
-    
-    
-    
-    // Override to support editing the table view.
+
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-             let entry = EntryController.sharedInstance.entries[indexPath.row]
+            guard let entry = EntryController.sharedInstance.fetchedResultsController.fetchedObjects?[indexPath.row] else { return }
             EntryController.sharedInstance.deleteEntry(entry:entry)
-            tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
     
     // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // IIDOO
         if segue.identifier == "toDetailView" {
-            
             guard let selectedIndexPath = tableView.indexPathForSelectedRow, let destinationVC = segue.destination as? EntryDetailViewController else { return}
             
-            let entry = EntryController.sharedInstance.entries[selectedIndexPath.row]
+            let entry = EntryController.sharedInstance.fetchedResultsController.fetchedObjects?[selectedIndexPath.row]
             
             destinationVC.entry = entry
         }
     }
+}
+
+extension EntryListTableViewController: NSFetchedResultsControllerDelegate {
+    
+    //conform to NSFetchedResultsControllerDelegate
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+            case .delete:
+                guard let indexPath = indexPath else { break }
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            case .insert:
+                guard let newIndexPath = newIndexPath else { break }
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            case .move:
+                guard let indexPath = indexPath, let newIndexPath = newIndexPath else { break }
+                tableView.moveRow(at: indexPath, to: newIndexPath)
+            case .update:
+                guard let indexPath = indexPath else { break }
+                tableView.reloadRows(at: [indexPath], with: .automatic)
+            
+            @unknown default:
+                fatalError()
+        }
+        
+    }
+    
+    
 }
